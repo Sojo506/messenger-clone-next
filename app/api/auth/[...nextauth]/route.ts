610ad884bda773b70bd26ae3cb49a8fc493c5e7f
Sojo506 <fabiansojowork@gmail.com>
2023-05-25 18:06:04 -0600

    nextauth setup

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,64 @@
+import bcrypt from "bcrypt"
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+
+import prisma from "@/app/libs/prismadb"
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { lable: "email", type: "text" },
+        password: { lable: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid Credentials')
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email
+          }
+        })
+
+        // Here we are going to check if the user got a credentials register, not using google or github
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid Credentials')
+        }
+
+        const isCorrectPassword = await bcrypt.compare(
+          credentials.password, user.hashedPassword
+        )
+
+        if (!isCorrectPassword) {
+          throw new Error('Invalid Credentials')
+        }
+
+        return user
+      }
+    }),
+  ],
+  debug: process.env.NODE_ENV === "development",
+  session: {
+    strategy: "jwt"
+  },
+  secret: process.env.NEXTAUTH_SECRET
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
