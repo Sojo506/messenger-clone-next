7427b03f161353a76677d685cf08037953893dea
Sojo506 <fabiansojowork@gmail.com>
2023-05-29 19:06:52 -0600

    users screen and conversations screen, conversation creation

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,9 +1,14 @@
+import getUsers from "../actions/getUsers";
 import Sidebar from "../components/sidebar/Sidebar";
+import UserList from "./components/UserList";
 
 const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers();
+
   return (
     // @ts-expect-error Server Component
     <Sidebar>
+      <UserList items={users} />
       <div className="h-full">{children}</div>
     </Sidebar>
   );
